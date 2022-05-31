4fcf926c2f5527f20fe7f4254a4ce54baa0f2d82
« Coline <c.ducourtieux@interface3.be>
2022-05-31 21:03:37 +0200

    added css. Added images in images. Added font folder

--- views/homePage.php
+++ views/homePage.php
@@ -1,9 +1,12 @@
-<h1> Home Page </h1>
-
-<div> </div>
-<div> </div>
-<div> </div>
-<div> </div>
-<div> </div>
-<div> </div>
-<div> </div>
+<div class="rawFruit">
+    <h4>Raw Fruits</h4>
+    <p>Raw fruits are those which are not ripe properly. 
+    But it also contains many different nutrition. The raw food diet encourages people to eat only raw (never heated), unprocessed foods. While eating more produce is advantageous, experts say there’s no reason to avoid cooking it.</p>
+</div>
+<div class="orange"> </div>
+<div class="fruitDiet"> </div>
+<div class="papaya"> </div>
+<div class="dragonfruit"> </div>
+<div class="pineapple"> </div>
+<div class="cherry"> </div>
+<div class="tangerine"> </div>
