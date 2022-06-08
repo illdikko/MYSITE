48827a4e34899ec5d7de8e574fbb7467ff8690a2
« Coline <c.ducourtieux@interface3.be>
2022-06-08 23:05:13 +0200

    updated fonts in fonts folder+modified CSS overall

--- views/homePage.php
+++ views/homePage.php
@@ -1,10 +1,18 @@
+<div class="title1"><h4>Healthy Eating</h4></div>
+<div class="title2"><h1>All About Fruits <br></h1></div>
 <div class="rawFruit">
     <h4>Raw Fruits</h4>
     <p>Raw fruits are those which are not ripe properly. 
-    But it also contains many different nutrition. The raw food diet encourages people to eat only raw (never heated), unprocessed foods. While eating more produce is advantageous, experts say there’s no reason to avoid cooking it.</p>
+    But it also contains many different nutrition. The raw food diet encourages people to eat only raw (never heated),
+     unprocessed foods. While eating more produce is advantageous, experts say there’s no reason to avoid cooking it.</p>
 </div>
 <div class="orange"> </div>
-<div class="fruitDiet"> </div>
+<div class="fruitDiet"> <h4>Fruit Diet</h4>
+<p>The fruitarian, or fruit, diet is a highly restrictive vegan diet.
+     It excludes all animal products, including dairy. 
+     People following this program eat a diet consisting primarily of raw fruits. 
+     Vegetables, dried fruits, nuts, and seeds can also be eaten in moderation.
+</p></div>
 <div class="papaya"> </div>
 <div class="dragonfruit"> </div>
 <div class="pineapple"> </div>
